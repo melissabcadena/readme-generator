2457b14ac511c9c18019a37328f538369363d55f
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-07-18 09:33:55 -0500

    add validation to each question

--- index.js
+++ index.js
@@ -7,53 +7,134 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'projectName',
-            message: 'What is the name of your project? (Required)'
+            message: 'What is the name of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your project name!');
+                  return false;
+                } 
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Please provide a description of your project. (Required)'
+            message: 'Please provide a description of your project. (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                  return true;
+                } else {
+                  console.log('Please provide a response!');
+                  return false;
+                }
+            }
         },
         {
             type: 'confirm',
             name: 'tableOfContents',
             default: 'true',
+            message: 'Would you like to add a Table of Contents?',
+            validate: tableOfContentsConfirm => {
+                if (tableOfContentsConfirm) {
+                  return true;
+                } else {
+                  console.log('Please confirm if you would like a table of contents!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'installation',
-            message: 'What steps are required to install your project. Pleae provide a step-by-step description of how to get the development environment running.'
+            message: 'What steps are required to install your project? Pleae provide a step-by-step description of how to get the development environment running. (Required)',
+            validate: installationInput => {
+                if (installationInput) {
+                  return true;
+                } else {
+                  console.log('Please provide a response!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'usage',
-            message: 'Please provide instructions and examples for use.'
+            message: 'Please provide instructions and examples for use. (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                  return true;
+                } else {
+                  console.log('Please provide a response!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'credits',
-            message: 'Please list your collaborators with links to their GitHub profiles.'
+            message: 'Please list your collaborators with links to their GitHub profiles. (Required)',
+            validate: creditsInput => {
+                if (creditsInput) {
+                  return true;
+                } else {
+                  console.log('Please provide a response!');
+                  return false;
+                }
+            }
         },
         {
             type: 'list',
             name: 'license',
-            message: 'Please choose a license for your application.',
-            choices: ['MIT', 'AUR', 'CPAN', 'CPAN/METACRAN', 'Crates.io', 'CTAN', 'Eclipse Marketplace', 'Hex.pm', 'NPM']
+            message: 'Please choose a license for your application. (Required)',
+            choices: ['MIT', 'AUR', 'CPAN', 'CPAN/METACRAN', 'Crates.io', 'CTAN', 'Eclipse Marketplace', 'Hex.pm', 'NPM'],
+            validate: licenseInput => {
+                if (licenseInput) {
+                  return true;
+                } else {
+                  console.log('Please provide a response!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'test',
-            message: 'Please write tests for your application and provide examples on how to run them.'
+            message: 'Please write tests for your application and provide examples on how to run them. (Required)',
+            validate: testInput => {
+                if (testInput) {
+                  return true;
+                } else {
+                  console.log('Please provide a response!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'githubName',
-            message: 'What is your GitHub username?'
+            message: 'What is your GitHub username? (Required)',
+            validate: gitHubInput => {
+                if (gitHubInput) {
+                  return true;
+                } else {
+                  console.log('Please provide a response!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'email',
-            message: 'What is your email address?'
+            message: 'What is your email address?',
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log('Please provide a response!');
+                  return false;
+                }
+            }
         }
     ])
 };
